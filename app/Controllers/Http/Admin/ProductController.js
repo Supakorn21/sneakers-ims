b80190935790d3b6b376824bb57bda5e2d38dd9d
Supakorn Charoen <supakorncharoen@gmail.com>
2023-01-08 16:36:33 +0700

    [02-products] - Inserting New Products

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -1,11 +1,26 @@
 "use strict";
+const Database = use("Database");
 
 class ProductController {
   index({ view }) {
     return view.render("admin/products/all");
   }
-  store({ view }) {
-    return "Hey";
+  async store({ request, response }) {
+    try {
+      const post = request.post();
+      await Database.raw(
+        `
+      INSERT INTO products (title, sku, material, description,
+        brand_id, qty,size,user_id)
+VALUES("${post.title}","${post.sku}","${post.material}","${post.description}",2,${post.qty},1,1)
+    `
+      );
+      return `<h1 style="color: green">Saved Success</h1>`;
+    } catch (error) {
+      console.log(error);
+      return `<h1 style="color: red">There was an error</h1>
+      <h3>${error.sqlMessage}</h3>`;
+    }
   }
   create({ view }) {
     return view.render("admin/products/create");

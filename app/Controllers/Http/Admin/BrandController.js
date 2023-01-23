8d62e4bcfa9fc663f3f59c196f71a340aa0c3a56
Supakorn Charoen <supakorncharoen@gmail.com>
2023-01-23 13:15:15 +0700

    [03-brands] - Duplicate what I did on products

---
+++ app/Controllers/Http/Admin/BrandController.js
@@ -0,0 +1,187 @@
+"use strict";
+
+const Database = use("Database");
+const sqlString = require("sqlstring");
+
+class BrandController {
+  async index({ view, response, request }) {
+    try {
+      // let title = post.title;
+      // let sku = post.sku;
+      // let material = post.material;
+      // let description = post.description;
+      // let qty = post.qty;
+      // let size = post.size;
+
+      let allProducts = await Database.raw(`
+      SELECT products.id, products.title,products.sku,
+      brands.title as brand, concat(users.f_name, " ", users.l_name )as user,
+      products.material,products.qty,products.size, products.user_id,
+      products.created_at
+      FROM products
+      INNER JOIN brands
+      ON products.brand_id = brands.id
+      INNER JOIN users
+      ON products.user_id = users.id ORDER BY created_at ASC
+      `);
+      allProducts = allProducts[0];
+
+      return view.render("admin/brands/all", { allProducts });
+    } catch (error) {
+      console.log(error);
+      // return response.redirect("back");
+    }
+  }
+  async store({ view, response, request }) {
+    try {
+      const post = request.post();
+      let title = post.title;
+      let sku = post.sku;
+      let material = post.material;
+      let description = post.description;
+      let qty = post.qty;
+      let size = post.size;
+      let img_url = post.img_url;
+      await Database.raw(
+        `
+   INSERT INTO products (
+    title,
+    sku,
+    img_url,
+    material,
+    description,
+    brand_id,
+    qty,
+    size,
+    user_id
+  )
+VALUES (
+    ${sqlString.escape(title)},
+    ${sqlString.escape(sku)},
+    ${sqlString.escape(img_url)},
+    ${sqlString.escape(material)},
+    ${sqlString.escape(description)},
+    ${parseInt(1)},
+    ${parseInt(qty)},
+    ${sqlString.escape(size)},
+    ${parseInt(1)}
+  )
+    `
+      );
+
+      return response.redirect("/admin/brands");
+    } catch (error) {
+      return response.redirect("back");
+    }
+  }
+  create({ view, response, request }) {
+    return view.render("admin/brands/create");
+  }
+  async show({ view, response, request, params }) {
+    try {
+      let product = await Database.raw(`
+      SELECT products.id, products.title,products.sku,products.img_url, products.description,
+      brands.title as brand, concat(users.f_name, " ", users.l_name )as user,
+      products.material,products.qty,products.size, products.user_id,
+      products.created_at
+      FROM products
+      INNER JOIN brands
+      ON products.brand_id = brands.id
+      INNER JOIN users
+      ON products.user_id = users.id
+      WHERE products.id = ${params.id}
+      ORDER BY created_at ASC
+      LIMIT 1
+      `);
+      product = product[0][0];
+
+      return view.render("admin/brands/show", { product });
+    } catch (error) {
+      console.log(error);
+      // return response.redirect("back");
+    }
+  }
+  async edit({ view, response, request, params }) {
+    try {
+      let product = await Database.raw(`
+      SELECT products.id, products.title,products.sku,products.img_url, products.description,
+      brands.title as brand, concat(users.f_name, " ", users.l_name )as user,
+      products.material,products.qty,products.size, products.user_id,
+      products.created_at
+      FROM products
+      INNER JOIN brands
+      ON products.brand_id = brands.id
+      INNER JOIN users
+      ON products.user_id = users.id
+      WHERE products.id = ${params.id}
+      ORDER BY created_at ASC
+      LIMIT 1
+      `);
+
+      product = product[0][0];
+
+      return view.render("admin/brands/edit", { product });
+    } catch (error) {
+      console.log(error);
+      // return response.redirect("back");
+    }
+  }
+  async update({ response, request, params }) {
+    try {
+      const post = request.post();
+      const id = params.id;
+      let title = post.title;
+      let sku = post.sku;
+      let material = post.material;
+      let description = post.description;
+      let qty = post.qty;
+      let size = post.size;
+      let img_url = post.img_url;
+      await Database.raw(
+        `
+   UPDATE products
+   SET
+   title = ${sqlString.escape(title)},
+   sku = ${sqlString.escape(sku)},
+   img_url = ${sqlString.escape(img_url)},
+   material = ${sqlString.escape(material)},
+   description = ${sqlString.escape(description)},
+   brand_id = ${parseInt(1)},
+   qty = ${parseInt(qty)},
+   size = ${sqlString.escape(size)},
+   user_id = ${parseInt(1)} 
+   WHERE id = ${id}
+   `
+      );
+
+      return response.redirect(`/admin/brands/${id}`);
+    } catch (error) {
+      return response.redirect("back");
+    }
+  }
+  async delete({ response, request, params }) {
+    try {
+      const post = request.post();
+      const id = params.id;
+      let title = post.title;
+      let sku = post.sku;
+      let material = post.material;
+      let description = post.description;
+      let qty = post.qty;
+      let size = post.size;
+      let img_url = post.img_url;
+      await Database.raw(
+        `
+        DELETE FROM products
+    WHERE id = ${id}
+   `
+      );
+
+      return response.redirect(`/admin/brands`);
+    } catch (error) {
+      return response.redirect("back");
+    }
+  }
+}
+
+module.exports = BrandController;
--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 
+// Admin/Products
 Route.get("/admin/products", "Admin/ProductController.index");
 Route.post("/admin/products", "Admin/ProductController.store");
 Route.get("/admin/products/create", "Admin/ProductController.create");
@@ -23,3 +24,12 @@ Route.get("/admin/products/:id", "Admin/ProductController.show");
 Route.get("/admin/products/:id/edit", "Admin/ProductController.edit");
 Route.put("/admin/products/:id", "Admin/ProductController.update");
 Route.get("/admin/products/:id/delete", "Admin/ProductController.delete");
+
+// Admin/Brands
+Route.get("/admin/brands", "Admin/BrandController.index");
+Route.post("/admin/brands", "Admin/BrandController.store");
+Route.get("/admin/brands/create", "Admin/BrandController.create");
+Route.get("/admin/brands/:id", "Admin/BrandController.show");
+Route.get("/admin/brands/:id/edit", "Admin/BrandController.edit");
+Route.put("/admin/brands/:id", "Admin/BrandController.update");
+Route.get("/admin/brands/:id/delete", "Admin/BrandController.delete");

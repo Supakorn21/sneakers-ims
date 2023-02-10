8c84a084ed6a7909c0a37f21ed949d8139995c52
Supakorn Charoen <supakorncharoen@gmail.com>
2023-02-10 19:08:46 +0700

    [05-dashboard] - Get data from mysql and Display to the chart

--- app/Controllers/Http/Admin/DashboardController.js
+++ app/Controllers/Http/Admin/DashboardController.js
@@ -1,34 +1,40 @@
 "use strict";
 
+const Database = use("Database");
+const sqlString = require("sqlstring");
+
 class DashboardController {
   async index({ view, response, request }) {
     try {
-      // let title = post.title;
-      // let sku = post.sku;
-      // let material = post.material;
-      // let description = post.description;
-      // let qty = post.qty;
-      // let size = post.size;
+      let data = {
+        topSalesMothly: {
+          months: [],
+          total_items: [],
+          total_earned: [],
+        },
+      };
+      let getMonthsSales = await Database.raw(`
+          SELECT
+          MONTHNAME(MIN(orders.created_at)) as month,
+          SUM(items.qty) as total_items,
+          SUM(items.price * items.qty) as total_price
+        FROM
+        orders
+          INNER JOIN items ON orders.id = items.order_id
+        WHERE
+          YEAR (orders.created_at) = YEAR(CURDATE())
+        GROUP BY
+          MONTH (orders.created_at);
+        `);
+      getMonthsSales = getMonthsSales[0];
 
-      //   let allBrands = await Database.raw(`
-      //         SELECT
-      //         brands.id,
-      //         brands.title,
-      //         brands.img_url,
-      //         brands.user_id,
-      //         concat(users.f_name,' ', users.l_name) as user,
-      //         brands.created_at,
-      //         brands.updated_at
-      //         FROM
-      //         brands
-      //         INNER JOIN users ON brands.user_id = users.id
-      //         ORDER BY
-      //         brands.title ASC
-      //   `);
-      //   allBrands = allBrands[0];
-      let allBrands = "";
+      getMonthsSales.map((item) => {
+        data.topSalesMothly.months.push(item.month);
+        data.topSalesMothly.total_items.push(item.total_items);
+        data.topSalesMothly.total_earned.push(item.total_price);
+      });
 
-      return view.render("admin/dashboard/index", { allBrands });
+      return view.render("admin/dashboard/index", { data });
     } catch (error) {
       console.log(error);
       // return response.redirect("back");
